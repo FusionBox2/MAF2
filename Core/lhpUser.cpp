6b225c4808f7bfc9253b8744a454dcd7033ec6f2
ior04 <ior04>
2007-12-19 13:56:21 +0000

    lhpUser with exeution script

--- Core/lhpUser.cpp
+++ Core/lhpUser.cpp
@@ -2,8 +2,8 @@
 Program:   Multimod Application Framework
 Module:    $RCSfile: lhpUser.cpp,v $
 Language:  C++
-Date:      $Date: 2007-12-18 14:35:32 $
-Version:   $Revision: 1.3 $
+Date:      $Date: 2007-12-19 13:56:21 $
+Version:   $Revision: 1.4 $
 Authors:   Daniele Giunchi
 ==========================================================================
 Copyright (c) 2002/2004
@@ -32,7 +32,8 @@ CINECA - Interuniversity Consortium (www.cineca.it)
 lhpUser::lhpUser()
 //----------------------------------------------------------------------------
 {
-  
+  m_PythonExe ="python.exe ";
+  m_PythonUploadFullPath  = (mafGetApplicationDirectory() + "\\VMEUploaderDownloader\\").c_str();
 }
 //----------------------------------------------------------------------------
 lhpUser::~lhpUser()
@@ -47,12 +48,77 @@ bool lhpUser::CheckUserCredentials()
   {
     InitializeUserInformations();
   }
-  bool res = true;// = ShowLoginDialog();
-  // Check only username and not password.
-  // This can be valid for anonymous (or guest) user without password
-  //res = !m_Username.IsEmpty() && res;
+  int res = ShowLoginDialog() != wxID_CANCEL;
+  if(!res) return res;
 
-  
+  res = res && !m_Username.IsEmpty() && !m_Password.IsEmpty() && ExecuteAuthenticationScript();
+  if(!res)
+  {
+    wxMessageBox("Incorrect Authentication");
+  }
 
   return res;
+}
+//----------------------------------------------------------------------------
+bool lhpUser::ExecuteAuthenticationScript()
+//----------------------------------------------------------------------------
+{
+  wxString oldDir = wxGetCwd();
+  mafLogMessage( _T("Current working directory is: '%s' "), wxGetCwd().c_str() );
+  wxSetWorkingDirectory(m_PythonUploadFullPath.GetCStr());
+  mafLogMessage( _T("Now current working directory is: '%s' "), wxGetCwd().c_str() );
+
+  // get manual tags
+  wxString command2execute;
+  command2execute.Clear();
+  command2execute = m_PythonExe;
+
+  command2execute.Append(" lhpAuthenticationControl.py ");
+  command2execute.Append(m_Username.GetCStr());
+  command2execute.Append(" ");
+  command2execute.Append(m_Password.GetCStr());
+
+  //mafLogMessage( _T("Executing command: '%s'"), command2execute.c_str() );
+
+  long pid = wxExecute(command2execute, wxEXEC_SYNC);
+
+  wxArrayString output;
+  wxArrayString errors;
+
+  pid = wxExecute(command2execute, output, errors);
+
+
+  mafLogMessage("Command Output Messages:");
+  for (int i = 0; i < output.size(); i++)
+  {
+    mafLogMessage(output[i]);
+  }
+
+  mafLogMessage("Command Errors Messages:");
+  for (int i = 0; i < errors.size(); i++)
+  {
+    mafLogMessage(errors[i]);
+  }
+
+  // gathering values from Python Output:
+
+  //if dictVC.IsDictionaryUpToDate() == True:
+  //print "UpToDate"
+  //else:
+  //print "NotUpToDate"
+
+  wxString result = output[output.size() - 1];
+
+  wxSetWorkingDirectory(oldDir);
+  mafLogMessage( _T("Current working directory is: '%s' "), wxGetCwd().c_str() );
+
+  if (result == "Authenticated")
+  {
+    return true;
+  } 
+  else 
+  {
+    return false;
+  }  
+
 }
--- Core/lhpUser.h
+++ Core/lhpUser.h
@@ -2,8 +2,8 @@
 Program:   Multimod Application Framework
 Module:    $RCSfile: lhpUser.h,v $
 Language:  C++
-Date:      $Date: 2007-12-18 12:08:38 $
-Version:   $Revision: 1.1 $
+Date:      $Date: 2007-12-19 13:56:21 $
+Version:   $Revision: 1.2 $
 Authors:   Daniele Giunchi
 ==========================================================================
 Copyright (c) 2002/2004
@@ -36,5 +36,11 @@ public:
   By default open Login Dialog if the user did not inserted any information.*/
   virtual bool CheckUserCredentials();
 
+protected:
+  bool ExecuteAuthenticationScript();
+
+  mafString m_PythonUploadFullPath; //>directory where the scripts are
+  mafString m_FileName; //>script file name
+  mafString m_PythonExe; //>python  executable
 };
 #endif
