4ef3ba823fe97790dd9a8f60c906b68f51e83607
ior04 <ior04>
2007-12-28 12:43:41 +0000

    add code to open python download page in the browser

--- Installer/PythonScriptVersion.py
+++ Installer/PythonScriptVersion.py
@@ -1,14 +1,22 @@
 import sys, time
+import updatePythonVersion
 
 #print sys.version       # version info in string
 pythonInfos = sys.version_info  # version info in tuple 
 
 #other controls
-version = str(pythonInfos[0]) + "." + str(pythonInfos[1]) + "." + str(pythonInfos[2])
+version = str(pythonInfos[0]) + "." + str(pythonInfos[1])# + "." + str(pythonInfos[2])
 print version
-if(version != "2.5.0"):
+
+#create file to prove that python exists in current machine
+file = open("pythonVersion.txt","w")
+file.write(version)	
+file.close() # this is icing, you can just exit and this will be
+
+if(version != "2.5"):
   print "############ Warning : Python Version must be 2.5.0 ##############"
   print "############ Several features can't work with different versions  ##############"
+  updatePythonVersion.openPythonDownloadPage()
   time.sleep(3)
 else:
   pass
@@ -17,6 +25,8 @@ try:
   import wx
 except:
   print "############ Warning : wxPython not present ##############"
+  print "############ Several features can't work without wxPython  ##############"
+  updatePythonVersion.openWxPythonDownloadPage()
   time.sleep(3)
 
 
