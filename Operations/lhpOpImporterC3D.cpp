04ca0da6ed0458281b87f18939f8f6978fdce9d2
moiseev <moiseev>
2008-02-11 16:59:41 +0000

    fix for latest problem..

--- Operations/lhpOpImporterC3D.cpp
+++ Operations/lhpOpImporterC3D.cpp
@@ -2,8 +2,8 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: lhpOpImporterC3D.cpp,v $
   Language:  C++
-  Date:      $Date: 2008-02-07 11:16:08 $
-  Version:   $Revision: 1.8 $
+  Date:      $Date: 2008-02-11 16:59:41 $
+  Version:   $Revision: 1.9 $
   Authors:   Daniele  Giunchi
 ==========================================================================
   Copyright (c) 2001/2005 
@@ -378,8 +378,8 @@ void lhpOpImporterC3D::Initialize(const mafString &fullFileName, lhpOpImporterC3
   intData.m_NumEvents = getEvents();              //events number
 
   //derived
-  intData.m_TrajectorySamplePeriod = ((double)intData.m_LengthMs/(double)intData.m_NumFrames) / 1000.0;
-  intData.m_AnalogSamplePeriod = ((double)intData.m_LengthMs/(double)intData.m_NumSamples) / 1000.0;
+  intData.m_TrajectorySamplePeriod = ((double)intData.m_LengthMs/(double)intData.m_NumFrames);
+  intData.m_AnalogSamplePeriod = ((double)intData.m_LengthMs/(double)intData.m_NumSamples);
   intData.m_VectogramSamplePeriod = intData.m_AnalogSamplePeriod;
 
   wxString fileName = fullFileName.GetCStr();
