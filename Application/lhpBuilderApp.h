62d773ee3c5a224362801593aa3d9f4a93984539
aqd0 <aqd0>
2008-03-26 13:28:19 +0000

    Changed #define name.

--- Application/lhpBuilderApp.h
+++ Application/lhpBuilderApp.h
@@ -2,15 +2,15 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: lhpBuilderApp.h,v $
   Language:  C++
-  Date:      $Date: 2007-08-22 14:01:40 $
-  Version:   $Revision: 1.1 $
+  Date:      $Date: 2008-03-26 13:28:19 $
+  Version:   $Revision: 1.2 $
   Authors:   Paolo Quadrani , Stefano Perticoni
 ==========================================================================
   Copyright (c) 2001/2005 
   CINECA - Interuniversity Consortium (www.cineca.it)
 =========================================================================*/
-#ifndef __testViewApp_H__
-#define __testViewApp_H__
+#ifndef __lhpBuilderApp_H__
+#define __lhpBuilderApp_H__
 //----------------------------------------------------------------------------
 // Include:
 //----------------------------------------------------------------------------
