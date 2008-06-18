59149c883873022b5b7a68a865dbb29f41b4a423
ior04 <ior04>
2008-06-18 12:13:50 +0000

    correct test in order to handle proxy and port

--- VMEUploaderDownloader/webServicesClient/MtomUploadURITest.py
+++ VMEUploaderDownloader/webServicesClient/MtomUploadURITest.py
@@ -4,8 +4,10 @@ import MtomUploadURI
 class MtomUploadURITest(unittest.TestCase):
     """"""
     def testUploadURI(self):
+        proxy = ""
+        port = 0
         instance = MtomUploadURI.MtomUploadURI()
-        result = instance.ListSrbDir('https://ws-lhdl-dev.cineca.it:12443/mafSRBUploadURI.cgi')
+        result = instance.ListSrbDir('https://ws-lhdl-dev.cineca.it:12443/mafSRBUploadURI.cgi', proxy, port)
         #self.assertEqual(, True)
         print "Test Upload URI"
         print result
