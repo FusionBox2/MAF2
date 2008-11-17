55b5d82020bde4fdf83449b5a9aafed7a417943b
lut01 <lut01>
2008-11-17 11:55:43 +0000

    BES: Added VME for deformation of muscles

---
+++ BES_Beta/Medical/Operations/medOpCreateMuscleWrapper.h
@@ -0,0 +1,45 @@
+/*=========================================================================
+  Program:   Multimod Application Framework
+  Module:    $RCSfile: medOpCreateMuscleWrapper.h,v $
+  Language:  C++
+  Date:      $Date: 2008-11-17 11:55:43 $
+  Version:   $Revision: 1.1.2.1 $
+  Authors:   Josef Kohout
+==========================================================================
+Copyright (c) 2002/2004
+CINECA - Interuniversity Consortium (www.cineca.it) 
+=========================================================================*/
+
+#ifndef __medOpCreateMuscleWrapper_H__
+#define __medOpCreateMuscleWrapper_H__
+
+#include "mafOp.h"
+
+//----------------------------------------------------------------------------
+// forward references :
+//----------------------------------------------------------------------------
+class medVMEMuscleWrapper;
+class mafGui;
+class mafEvent;
+//----------------------------------------------------------------------------
+// medOpCreateMuscleWrapper :
+//----------------------------------------------------------------------------
+/** */
+class medOpCreateMuscleWrapper: public mafOp
+{
+public:
+  medOpCreateMuscleWrapper(const wxString &label = "CreateMuscleWrappedMeter");
+  ~medOpCreateMuscleWrapper(); 
+
+  mafTypeMacro(medOpCreateMuscleWrapper, mafOp);
+
+  mafOp* Copy();
+
+  bool Accept(mafNode *node);
+  void OpRun();
+  void OpDo();
+
+protected: 
+  medVMEMuscleWrapper *m_Meter;
+};
+#endif //__medOpCreateMuscleWrapper_H__
