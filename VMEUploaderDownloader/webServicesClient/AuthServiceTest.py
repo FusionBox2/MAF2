4ec55809c0a8355651fa848cefede9603737af0c
cvsbiom1 <cvsbiom1>
2008-12-02 13:21:53 +0000

    *** empty log message ***

---
+++ VMEUploaderDownloader/webServicesClient/AuthServiceTest.py
@@ -0,0 +1,22 @@
+import unittest
+import xmlrpcDemoWS
+import os, commands, md5
+
+class AuthServiceTest(unittest.TestCase):
+    """"""
+    def test(self):
+
+        ws = xmlrpcDemoWS.xmlrpc_demoWS()
+
+        res = "<?xml version='1.0'?>\n<methodResponse>\n<params>\n<param>\n<value><boolean>1</boolean></value>\n</param>\n</params>\n</methodResponse>\n"
+
+        print "auth service"
+        out = ws.run('authservice')
+        self.assertEqual(out[0], True)
+        self.assertEqual(out[1], res)
+        #print out[1]
+        print "done!"
+
+
+if __name__ == '__main__':
+    unittest.main()
