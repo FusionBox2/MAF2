0084c487c8d41e4ebc417db1ad2ef70a524f2362
mucci <mucci>
2008-12-12 16:21:26 +0000

    Correct Error

--- VMEUploaderDownloader/DownloadHandler.py
+++ VMEUploaderDownloader/DownloadHandler.py
@@ -194,39 +194,39 @@ class DownloadHandler:
             print "IsLast: " + str(self.isLast)
         if (self.isLast == "false"):
             #replace status for resource correctly downloaded
-            if(os.path.exists(sys.path[0] + '\\status.lhp')):
-                while 1:
-                    size = os.path.getsize(sys.path[0] + '\\status.txt')
-                    statusFile = open(sys.path[0] + '\\status.txt', 'a')
-                    try:
-                        msvcrt.locking(statusFile.fileno(), msvcrt.LK_RLCK, size)
-                        statusFile.write('ended\n')
-                        statusFile.close()
-                        break
-                    except:
-                        counter = counter+1 #to avoid deadlock
-                        statusFile.close()
-                        pass
-                    if(counter == 10):
-                        if Debug:
-                            print "----------Can not read in status.txt-----------"
-                        pass
-        else:
             if(os.path.exists(sys.path[0] + '\\status.txt')):
-                while 1:
+                  while 1:
+                        size = os.path.getsize(sys.path[0] + '\\status.txt')
+                        statusFile = open(sys.path[0] + '\\status.txt', 'a')
+                        try:
+                            msvcrt.locking(statusFile.fileno(), msvcrt.LK_RLCK, size)
+                            statusFile.write('ended\n')
+                            statusFile.close()  
+                            break                                      
+                        except:
+                            counter = counter+1 #to avoid deadlock
+                            statusFile.close()
+                            if(counter == 10):
+                                if Debug:
+                                  print "----------Can not read in status.lhp-----------"
+                                pass
+                        
+        else:
+          if(os.path.exists(sys.path[0] + '\\status.txt')):
+              while 1:
                     size = os.path.getsize(sys.path[0] + '\\status.txt')
                     statusFile = open(sys.path[0] + '\\status.txt', 'a')
                     try:
                         msvcrt.locking(statusFile.fileno(), msvcrt.LK_RLCK, size)
                         statusFile.write('lastEnded\n')
-                        statusFile.close()
+                        statusFile.close()  
                         break                                      
                     except:
                         counter = counter+1 #to avoid deadlock
                         statusFile.close()
                         if(counter == 10):
                             if Debug:
-                                  print "----------Can not read in status.lhp-----------"
+                              print "----------Can not read in status.lhp-----------"
                             pass
         
         processStarted = 0
