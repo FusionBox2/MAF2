bc15a5362a92f57fd58d5bb0a6124f42f47fa148
ior01 <ior01>
2009-02-13 14:15:54 +0000

    fixed upload bug and improved documentation

--- VMEUploaderDownloader/downloadSingleXML.py
+++ VMEUploaderDownloader/downloadSingleXML.py
@@ -7,10 +7,6 @@ from Debug import Debug
 
 class downloadSingleXML():
     def __init__(self):
-        self.user = sys.argv[0]
-        self.password = sys.argv[1]
-        self.fileToDownload = sys.argv[2]
-        self.incomingCacheDir = str(sys.argv[3]).replace("?", " ")
         
         self.datasetSRBURI = 'testSRBURI'
         self.datasetFileSize = -1
@@ -57,31 +53,45 @@ class downloadSingleXML():
         pass
 
 def main():
-    
-    if(len(sys.argv) != 5): #for test
-       sys.argv = []
-       sys.argv.append("Program")
-       sys.argv.append("testuser") #substitute
-       sys.argv.append("6w8DHF") #substitute
-       sys.argv.append('dataresource-8660') #xml test present in repository
-       sys.argv.append(os.getcwd()+"\\testDownload\\")
        
     if(len(sys.argv) == 5):
         sys.argv = sys.argv[1:]
         #print sys.argv
         dsXML = downloadSingleXML()
+        dsXML.user = sys.argv[0]
+        dsXML.password = sys.argv[1]
+        dsXML.fileToDownload = sys.argv[2]
+        dsXML.incomingCacheDir = str(sys.argv[3]).replace("?", " ")
         dsXML.downloadXMLFromBasket()
-        
         dsXML.datasetSRBURI = dsXML.retrieveTagValue('L0000_resource_data_Dataset_DatasetURI')
-        dsXML.datasetFileSize = dsXML.retrieveTagValue('L0000_resource_data_Size_FileSize')
-        
+        dsXML.datasetFileSize = dsXML.retrieveTagValue('L0000_resource_data_Size_FileSize')        
         dsXML.moveFileInIncomingCacheDirectory()
         
-        print "dtatset SRB Uri: " + dsXML.datasetSRBURI
-        print "dataset file size: "  + dsXML.datasetFileSize
         
-        assert(dsXML.datasetSRBURI == "data_4085")
-        assert(dsXML.datasetFileSize == "437")
-
+        # datatset SRB Uri: BEWARE !!! This print value is used by the Client application!
+        # DO NOT CHANGE! 
+        print dsXML.datasetSRBURI
+        
+        # dataset file size: BEWARE !!! This print value is used by the Client application!
+        # DO NOT CHANGE!  
+        print dsXML.datasetFileSize
+        
+    else:
+        print """
+        Usage: 
+        downloadSingleXML.py
+        testuser # substitute user name
+        6w8DHF # substitute user password
+        dataresource-8660 # substitute xml test data present in the repository
+        os.getcwd()+"\\testDownload\\" # substitute incoming cache dir
+       
+        Output:
+        # datatset SRB Uri: BEWARE !!! This print value is used by the Client application!
+        datasetSRBURI
+        
+        # dataset file size: BEWARE !!! This print value is used by the Client application!
+        datasetFileSize
+       
+       """
 if __name__ == '__main__':
     main()
--- VMEUploaderDownloader/downloadSingleXMLTest.py
+++ VMEUploaderDownloader/downloadSingleXMLTest.py
@@ -1,6 +1,14 @@
+#-----------------------------------------------------------------------------
+# BEWARE!!! This is mostly a prototype!!!
+# code is changing very fast so don't rely on it :P
+# author: Stefano Perticoni <s.perticoni@scsolutions.it>
+#-----------------------------------------------------------------------------
+
+
 import os
 import downloadSingleXML
 import unittest
+import sys
 
 class downloadSingleXMLTest(unittest.TestCase):
     
@@ -17,8 +25,36 @@ class downloadSingleXMLTest(unittest.TestCase):
         #2 pwd
         #3 xml file name to download
         #4 where download it
-        downloadSingleXML.main()        
+        d = downloadSingleXML.downloadSingleXML()
+        
+        d.user = "testuser"
+        d.password = "6w8DHF"
+        d.fileToDownload = 'dataresource-8660'
+        d.incomingCacheDir = str(os.getcwd()+"\\testDownload\\")
+      
+        d.downloadXMLFromBasket()
+        
+        d.datasetSRBURI = d.retrieveTagValue('L0000_resource_data_Dataset_DatasetURI')
+        d.datasetFileSize = d.retrieveTagValue('L0000_resource_data_Size_FileSize')
+        
+        d.moveFileInIncomingCacheDirectory()
+
+        assert(d.datasetSRBURI == "data_4085")
+        assert(d.datasetFileSize == "437")
+
         self.assertTrue(True)
         
+    def testMain(self):
+          
+       sys.argv = []
+       sys.argv.append("downloadSingleXML.py")
+       sys.argv.append("testuser") #substitute
+       sys.argv.append("6w8DHF") #substitute
+       sys.argv.append('dataresource-8660') #xml test present in repository
+       sys.argv.append(os.getcwd()+"\\testDownload\\")
+       sys.argv.append(os.getcwd()+"\\testDownload\\")
+       result = downloadSingleXML.main()
+       
+       
 if __name__ == '__main__':
     unittest.main()
