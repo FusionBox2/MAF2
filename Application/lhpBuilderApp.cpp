8e30624c23da2697bc0cc60485a9974c5c6ee666
mucci <mucci>
2009-02-23 13:29:32 +0000

    corrected "form" in "from"

--- Application/lhpBuilderApp.cpp
+++ Application/lhpBuilderApp.cpp
@@ -2,8 +2,8 @@
   Program:   Multimod Application Framework
   Module:    $RCSfile: lhpBuilderApp.cpp,v $
   Language:  C++
-  Date:      $Date: 2009-02-06 09:17:23 $
-  Version:   $Revision: 1.71.2.13 $
+  Date:      $Date: 2009-02-23 13:29:32 $
+  Version:   $Revision: 1.71.2.14 $
   Authors:   Paolo Quadrani , Stefano Perticoni
 ==========================================================================
   Copyright (c) 2001/2005 
@@ -415,7 +415,7 @@ mafPlugPipe<medPipeComputeWrapping>("Pipe to Visualize Compute Wrapping Meter");
   m_Logic->Plug(new lhpOpUploadMultiVME("Upload VME"),"Manage");
   m_Logic->Plug(new lhpOpEditTag("Edit Tag VME"),"Manage");
   m_Logic->Plug(new lhpOpDownloadVME("Download VME", lhpOpDownloadVME::FROM_BASKET),"Manage");
-  m_Logic->Plug(new lhpOpDownloadVME("Download VME form sandbox",lhpOpDownloadVME::FROM_SANDBOX),"Manage");
+  m_Logic->Plug(new lhpOpDownloadVME("Download VME from sandbox",lhpOpDownloadVME::FROM_SANDBOX),"Manage");
   
   m_Logic->Plug(new mafOpDecomposeTimeVarVME("Decompose Time"),"Create/Derive");
   m_Logic->Plug(new mafOpLabelExtractor("Extract Label"),"Create/Derive");
