fd1947979785569fda4bebb451c2507d8976997d
Fedor <fedormsv@gmail.com>
2015-03-31 12:19:05 +0200

    mafNodeManager and mafVMEManager are synchronized

--- Core/mafNodeManager.cpp
+++ Core/mafNodeManager.cpp
@@ -24,394 +24,108 @@
 //----------------------------------------------------------------------------
 
 
-#ifdef _MSC_FULL_VER
-#include <xiosbase>
-#endif
-
-#include <wx/busyinfo.h>
-#include <wx/fs_zip.h>
-#include <wx/zstream.h>
-#include <wx/ffile.h>
-#include "mafDecl.h"
-//#include "mafVmeData.h"
 #include "mafNodeManager.h"
+#include "mafDecl.h"
 #include "mafNode.h"
-//#include "mafNodeStorage.h"
 #include "mafNodeIterator.h"
-//#include "vtkTagItem.h"
+#include "mafEvent.h"
 
 //----------------------------------------------------------------------------
 mafNodeManager::mafNodeManager()
 //----------------------------------------------------------------------------
 {
-  m_Modified      = false;
-	m_MakeBakFile = true;
-//@@	m_storage   = NULL;
-	m_Root          = NULL;
-
-	m_MsfDir   = mafGetApplicationDirectory();
-  m_MsfDir   += "/Data/MSF/";
-	m_MsfFile   = "";
-	m_ZipFile   = "";
-	m_MergeFile = "";
-	m_Wildc     = "Multimod Storage Format file (*.msf)|*.msf";
-
-  m_Config = wxConfigBase::Get();
+  m_Modified = false;
+  m_Root     = NULL;
 }
 //----------------------------------------------------------------------------
 mafNodeManager::~mafNodeManager()
 //----------------------------------------------------------------------------
 {
-  if(m_Root) NotifyRemove( (mafNode*)m_Root ); // //SIL. 11-4-2005:  - cast root to node -- maybe to be removed
-  mafDEL(m_Root);
-
-  m_AppStamp.clear();
-
-  //@@ if(m_storage)	m_storage->Delete();
-  delete m_Config;  
+  SetRoot(NULL);
+  SetListener(NULL);
 }
 //----------------------------------------------------------------------------
 void mafNodeManager::OnEvent(mafEventBase *maf_event)
 //----------------------------------------------------------------------------
 {
-   //maf_event->Log();
-}
-//----------------------------------------------------------------------------
-void mafNodeManager::MSFNew(bool notify_root_creation)   
-//----------------------------------------------------------------------------
-{
-  m_Modified = false;
-  if(m_Root) NotifyRemove( (mafNode*) m_Root);  //SIL. 11-4-2005: cast root to node - maybe to be removed
-  mafDEL(m_Root);
-  mafNEW(m_Root);
-  m_Root->SetName("root");
-
-  //@@ if(m_storage) m_storage->Delete(); 
-  //@@ m_storage = mafNodeStorage::New();
-  //@@ m_Root = m_storage->GetOutput();
-  //@@ m_Root->SetName("root");
-
-  if(notify_root_creation)
-	{
-		//Add the application stamps
-		//vtkTagItem tag_appstamp;
-		//tag_appstamp.SetName("APP_STAMP");
-		//tag_appstamp.SetValue(this->m_AppStamp.c_str());
-		//m_Root->GetTagArray()->AddTag(tag_appstamp);
-		mafEventMacro(mafEvent(this,VME_ADDED,m_Root));
-		mafEventMacro(mafEvent(this,VME_SELECTED,m_Root)); 
-	}
-
-  m_MsfFile = ""; //next MSFSave will ask for a filename
-}
-//----------------------------------------------------------------------------
-void mafNodeManager::MSFOpen(int file_id)   
-//----------------------------------------------------------------------------
-{
-  /*
-  int idx = file_id - wxID_FILE1;
-	wxString file = "";
-	file = m_FileHistory.GetHistoryFile(idx);
-	MSFOpen(file);
-  */
-}
-//----------------------------------------------------------------------------
-void mafNodeManager::MSFOpen(wxString filename)   
-//----------------------------------------------------------------------------
-{
-	/*
-  wxWindowDisabler disableAll; //SIL. 17-9-2004: 
-
-  wxBusyCursor wait_cursor;
-  
-  if(!::wxFileExists(filename))
-	{
-		wxString msg("File ");
-		msg += filename;
-		msg += " not found!";
-		wxMessageBox( msg,"Warning", wxOK|wxICON_WARNING , NULL);
-		return;
-	}
-
-  // insert and select the root - reset m_msffile 
-  MSFNew(false);
-
-  wxString path, name, ext;
-  wxSplitPath(filename,&path,&name,&ext);
-  if(ext == "zip")
+  if (maf_event->GetChannel()!=MCH_UP)
+    return;
+  // events coming from the tree...
+  if(maf_event->GetId() == NODE_ATTACHED_TO_TREE)
   {
-    ZIPOpen(filename);
+    NotifyAdd((mafNode *)maf_event->GetSender());
+    MSFModified(true);
     return;
   }
-  
-  m_msffile = filename; 
+  if(maf_event->GetId() == NODE_DETACHED_FROM_TREE)
+  {
+    NotifyRemove((mafNode *)maf_event->GetSender());
+    MSFModified(true);
+    return;
+  }
+  mafEventMacro(*maf_event);
+}
 
-  wxBusyInfo wait("Loading MSF: Please wait");
-  m_storage->SetFileName(m_msffile.c_str());
- 
-  int res = m_storage->Load();  //modified by Stefano 29-10-2004
+//----------------------------------------------------------------------------
+mafNodeRoot *mafNodeManager::GetRoot()
+//----------------------------------------------------------------------------
+{
+  return m_Root;
+}
 
-  mflTimeStamp b[2];
-  m_Root->GetTimeBounds(b);
-  m_Root->SetTreeTime(b[0]);
-  m_Root->SetName("root");
-
-	////////////////////////////////  Application Stamp managing ////////////////////
-	if(m_Root->GetTagArray()->FindTag("APP_STAMP") == -1)
-	{
-		//update the old data files to support Application Stamp
-		vtkTagItem tag_appstamp;
-		tag_appstamp.SetName("APP_STAMP");
-		tag_appstamp.SetValue(this->m_AppStamp.c_str());
-		m_Root->GetTagArray()->AddTag(tag_appstamp);
-	}
-	
-	wxString app_stamp(m_Root->GetTagArray()->GetTag("APP_STAMP")->GetValue());
-	if(app_stamp == "INVALID" || ((app_stamp != m_AppStamp) && (m_AppStamp != "DataManager") && (m_AppStamp != "OPEN_ALL_DATA")))
-	{
-		//Application stamp not valid
-		wxMessageBox("File not valid for this application!","Warning", wxOK, NULL);
-		m_modified = false;
-		m_storage->Delete();
-		m_Root = NULL;
-		m_storage = NULL;
-		MSFNew();
-		return;
-	}
-	///////////////////////////////////////////////////////////////////////////////// 
-
+//----------------------------------------------------------------------------
+bool mafNodeManager::SetRoot(mafNodeRoot *root)
+//----------------------------------------------------------------------------
+{
+  NotifyRemove(m_Root);
+  if(m_Root)
+    m_Root->SetListener(NULL);
+  m_Root = root;
+  if(m_Root)
+    m_Root->SetListener(this);
   NotifyAdd(m_Root);
-
-  // modified by Stefano 29-10-2004 (beg)
-  // if some problems occurred during import give feedback to the user
-  if (res == 1)
-  {
-    wxString tmpStr("Errors during file parsing! Look the log area for error messages.");
-    wxMessageBox(tmpStr, "Error", wxOK | wxICON_ERROR);
-  }
-  //modified by Stefano 29-10-2004 (end)
-
-
-	mafEventMacro(mafEvent(this,VME_SELECTED,m_Root)); 
-  mafEventMacro(mafEvent(this,CAMERA_RESET)); 
-
-	m_FileHistory.AddFileToHistory(m_msffile);
-	m_FileHistory.Save(*m_Config);
-  */
-}
-//----------------------------------------------------------------------------
-void mafNodeManager::ZIPOpen(wxString filename)
-//----------------------------------------------------------------------------
-{
-  /*
-  wxBusyInfo wait("Unzipping file: Please wait");
-
-  m_zipfile = filename;
-  wxString file, ext, working_dir = "";
-  wxSplitPath(m_zipfile,&working_dir,&file,&ext);
-  if (working_dir == "")
-    working_dir = ::wxGetCwd();
-  
-  working_dir = working_dir + "\\~TmpData";
-  if (!wxDirExists(working_dir))
-    wxMkdir(working_dir);
-  wxSetWorkingDirectory(working_dir);
-
-  wxString path, name, complete_name, zfile, out_file;
-  wxSplitPath(m_zipfile,&path,&name,&ext);
-  complete_name = name + "." + ext;
-  
-  wxFSFile *zfileStream;
-  wxZlibInputStream *zip_is;
-  wxString pkg = "#zip:";
-  wxString header_name = complete_name + pkg;
-  int length_header_name = header_name.Length();
-  bool enable_mid = false;
-  wxFileSystem *zip_fs = new wxFileSystem();
-  zip_fs->AddHandler(new wxZipFSHandler);
-  zip_fs->ChangePathTo(m_zipfile);
-  // extract filename from the zip archive
-  zfile = zip_fs->FindFirst(complete_name+pkg+name+"\\*.*");
-  if (zfile == "")
-  {
-    enable_mid = true;
-    zfile = zip_fs->FindFirst(complete_name+pkg+"\\*.*");
-  }
-  wxSplitPath(zfile,&path,&name,&ext);
-  complete_name = name + "." + ext;
-  if (enable_mid)
-    complete_name = complete_name.Mid(length_header_name);
-  zfileStream = zip_fs->OpenFile(zfile);
-  zip_is = (wxZlibInputStream *)zfileStream->GetStream();
-  out_file = working_dir + "\\" + complete_name;
-  char *buf;
-  int s_size;
-#ifdef _MSC_FULL_VER
-  ofstream out_file_stream;
-#else
-  std::ofstream out_file_stream;
-#endif
-  if(ext == "msf")
-  {
-    m_msffile = out_file;
-    out_file_stream.open(out_file, std::ios_base::out);
-  }
-  else
-    out_file_stream.open(out_file, std::ios_base::binary);
-  s_size = zip_is->StreamSize();
-  buf = new char[s_size];
-  zip_is->Read(buf,s_size);
-  out_file_stream.write(buf, s_size);
-  out_file_stream.close();
-  delete[] buf;
-  
-  while ((zfile = zip_fs->FindNext()) != "")
-  {
-    zfileStream = zip_fs->OpenFile(zfile);
-    zip_is = (wxZlibInputStream *)zfileStream->GetStream();
-    wxSplitPath(zfile,&path,&name,&ext);
-    complete_name = name + "." + ext;
-    if (enable_mid)
-      complete_name = complete_name.Mid(length_header_name);
-    out_file = working_dir + "\\" + complete_name;
-    if(ext == "msf")
-    {
-      m_msffile = out_file;
-      out_file_stream.open(out_file, std::ios_base::out);
-    }
-    else
-      out_file_stream.open(out_file, std::ios_base::binary);
-    s_size = zip_is->StreamSize();
-    buf = new char[s_size];
-    zip_is->Read(buf,s_size);
-    out_file_stream.write(buf, s_size);
-    out_file_stream.close();
-    delete[] buf;
-  }
-  
-  zfileStream->UnRef();
-  delete zfileStream;
-  zip_fs->CleanUpHandlers();
-  delete zip_fs;
-  
-  if (m_msffile == "")
-  {
-    wxMessageBox("zip archive is not a valid msf file!", "Error");
-    return;
-  }
-
-  wxSetWorkingDirectory(working_dir);
-  MSFOpen(m_msffile);
-  */
-}
-//----------------------------------------------------------------------------
-void mafNodeManager::MSFSave()   
-//----------------------------------------------------------------------------
-{
-  /*
-  if(m_msffile == "") 
-  {
-    wxString file = mafGetSaveFile(m_msf_dir, m_wildc);
-    if(file == "") return;
-		if(!wxFileExists(file))
-		{
-			wxString path, name, ext, file_dir;
-			wxSplitPath(file,&path,&name,&ext);
-			file_dir = path + "\\" + name;
-			if(!wxDirExists(file_dir))
-				wxMkdir(file_dir);
-			file = file_dir + "\\" + name + "." + ext;
-		}
-    m_msffile = file;
-  }
-  if(wxFileExists(m_msffile) && m_make_bak_file)
-	{
-		wxString bak_filename = m_msffile + ".bak";
-		ifstream old_msf_file(m_msffile.c_str(), ios::in|ios::binary);
-		ofstream bak_msf_file(bak_filename.c_str(),ios::out|ios::binary);
-		bak_msf_file << old_msf_file.rdbuf();
-		old_msf_file.close();
-		bak_msf_file.close();
-	}
-	
-	wxBusyInfo wait("Saving MSF: Please wait");
-  m_storage->SetFileName(m_msffile.c_str());
-  if (m_storage->Write()!=0)
-  {
-    mafLogMessage("Error during MSF saving");
-  }
-	m_FileHistory.AddFileToHistory(m_msffile );
-	m_FileHistory.Save(*m_Config);
-  m_modified = false;
-  */
-}
-//----------------------------------------------------------------------------
-void mafNodeManager::MSFSaveAs()   
-//----------------------------------------------------------------------------
-{
-  /**
-   m_msffile = "";
-   MSFSave();
-   */
+  return true;
 }
 //----------------------------------------------------------------------------
 void mafNodeManager::VmeAdd(mafNode *n)
 //----------------------------------------------------------------------------
 {
-  if(n != NULL)
+  if(n == NULL)
+    return ;
+  // check the node's parent
+  mafNode *vp = n->GetParent();  
+  if(vp != NULL && !m_Root->IsInTree(vp))
   {
-    mafNode *vp = n->GetParent();  
-    assert( vp == NULL || m_Root->IsInTree(vp) );
-    if(vp == NULL) 
-			n->ReparentTo(m_Root);
-    NotifyAdd(n);
-    m_Modified = true;
+    assert(false);
+    return;
   }
+  if(vp == NULL) 
+    n->ReparentTo(m_Root); // reparent the node to the root
+  m_Modified = true;
 }
 //----------------------------------------------------------------------------
 void mafNodeManager::VmeRemove(mafNode *n)
 //----------------------------------------------------------------------------
 {
-  if(n != NULL && m_Root /*&& m_Root->IsInTree(n)*/) 
+  if(n == NULL)
+    return ;
+  if(!m_Root || !m_Root->IsInTree(n))
   {
-    NotifyRemove(n);
-    n->ReparentTo(NULL); // may kill the vme
-    m_Modified = true;
+    assert(false);
+    return;
   }
-}
-//----------------------------------------------------------------------------
-void mafNodeManager::TimeSet(double time)
-//----------------------------------------------------------------------------
-{
-  //if(m_Root) m_Root->SetTreeTime(time);
-}
-//----------------------------------------------------------------------------
-void mafNodeManager::TimeGetBounds(double *min, double *max)
-//----------------------------------------------------------------------------
-{
-  /*
-  mflTimeStamp b[2];
-  if(m_Root) 
-  {
-    m_Root->GetTimeBounds(b);
-    *min = b[0];
-    *max = b[1];
-  }
-  else
-  
-  {*/
-    *min = 0;
-    *max = 0;
-  //}
+  n->ReparentTo(NULL);
+  m_Modified = true;
 }
 //----------------------------------------------------------------------------
 void mafNodeManager::NotifyRemove(mafNode *n)
 //----------------------------------------------------------------------------
 {
   mafNodeIterator *iter = n->NewIterator();
-  iter->SetTraversalModeToPostOrder();
+  iter->IgnoreVisibleToTraverse(true); // ignore visible to traverse flag and visits all nodes
+  iter->SetTraversalModeToPostOrder(); // traverse is: first the subtree left to right, then the root
   for (mafNode *node = iter->GetFirstNode(); node; node = iter->GetNextNode())
-		mafEventMacro(mafEvent(this, VME_REMOVING, node));
+		mafEventMacro(mafEvent(this,VME_REMOVING,node)); // raise notification event (to logic)
   iter->Delete();
 }
 //----------------------------------------------------------------------------
@@ -419,67 +133,11 @@ void mafNodeManager::NotifyAdd(mafNode *n)
 //----------------------------------------------------------------------------
 {
   mafNodeIterator *iter = n->NewIterator();
+  iter->IgnoreVisibleToTraverse(true); // ignore visible to traverse flag and visits all nodes
   for (mafNode *node = iter->GetFirstNode(); node; node = iter->GetNextNode())
-    mafEventMacro(mafEvent(this,VME_ADDED,node));
+  {
+    mafEventMacro(mafEvent(this,VME_ADDED,node)); // raise notification event (to logic)
+  }
   iter->Delete();
-}
-//----------------------------------------------------------------------------
-void mafNodeManager::SetFileHistoryMenu(wxMenu *menu)
-//----------------------------------------------------------------------------
-{
-	m_FileHistory.UseMenu(menu);
-	m_FileHistory.Load(*m_Config);
-}
-//----------------------------------------------------------------------------
-bool mafNodeManager::AskConfirmAndSave()
-//----------------------------------------------------------------------------
-{
-  bool go = true;
-	if (m_Modified)
-	{
-		int answer = wxMessageBox("your work is modified, would you like to save it?","Confirm",
-			                     wxYES_NO|wxCANCEL|wxICON_QUESTION , mafGetFrame());
-		if(answer == wxCANCEL) go = false;
-		if(answer == wxYES)    MSFSave();
-	}
-	return go;
-}
-//----------------------------------------------------------------------------
-void mafNodeManager::SetApplicationStamp(const mafString& appstamp)
-//----------------------------------------------------------------------------
-{
-  m_AppStamp.push_back(appstamp);
-}
-//----------------------------------------------------------------------------
-void mafNodeManager::SetApplicationStamp(const std::vector<mafString>& appstamp)
-//----------------------------------------------------------------------------
-{
-  for (int i=0; i<appstamp.size();i++)
-  {
-    m_AppStamp.push_back(appstamp.at(i));
-  }
-}
-//----------------------------------------------------------------------------
-void mafNodeManager::UpdateFromTag(mafNode *n)
-//----------------------------------------------------------------------------
-{
-  /*
-  if (n)
-  {
-    mafVmeData *vd = (mafVmeData *)n->GetClientData();
-    if (vd)
-      vd->UpdateFromTag();
-  }
-  else
-  {
-    mafNodeIterator *iter = m_Root->NewIterator();
-    for (mafNode *n = iter->GetFirstNode(); n; n = iter->GetNextNode())
-    {
-      mafVmeData *vd = (mafVmeData *)n->GetClientData();
-      if (vd)
-        vd->UpdateFromTag();
-    }
-    iter->Delete();
-  }
-  */
+  m_Modified = true;
 }
--- Core/mafNodeManager.h
+++ Core/mafNodeManager.h
@@ -19,12 +19,10 @@
 //----------------------------------------------------------------------------
 // includes :
 //----------------------------------------------------------------------------
-#include "mafEvent.h"
-#include "mafEventSender.h"
 #include "mafBaseEventHandler.h"
+#include "mafEventSender.h"
 #include "mafNode.h"
 #include "mafNodeRoot.h"
-#include "mafGUIFileHistory.h"
 
 #ifdef MAF_EXPORTS
 #include "mafDllMacros.h"
@@ -34,38 +32,26 @@ EXPORT_STL_VECTOR(MAF_EXPORT,wxString);
 //----------------------------------------------------------------------------
 // Forward References :
 //----------------------------------------------------------------------------
-class mafNodeStorage;
 
-//----------------------------------------------------------------------------
-// mafNodeManager :
+/** mafNodeManager : Class managing nodes inside a MAF application.
+
+This class is responsible for managing the nodes in a MAF application:
+It can add or remove VME to the current tree, add a new tree.
+
+*/
 //----------------------------------------------------------------------------
 class MAF_EXPORT mafNodeManager: public mafBaseEventHandler, public mafEventSender
+//----------------------------------------------------------------------------
 {
 public:
-    mafNodeManager();
+  /** constructor */
+  mafNodeManager();
+
+  /** destructor */
   ~mafNodeManager(); 
 
+  /** Process events coming from other components */
   void OnEvent(mafEventBase *maf_event);
- 
-  /** 
-  Destroy all nodes (olso the root), for each destroyed node the manager send 
-  an event VME_REMOVING, then search the root and select it. */ 
-  void MSFNew   (bool notify_root_creation = true);
-  
-	/** Open the msf filename. */
-	void MSFOpen(wxString filename);
-  
-	/** Open the msf from file history. */
-  void MSFOpen(int file_id);
-
-  void ZIPOpen(wxString filename);
-  
-	/** Save the msf tree. */
-  void MSFSave();
-  
-	/** Save the msf tree with different filename. */
-  void MSFSaveAs();
-  
 	/** Return true if the tree has been modifyed. */
   bool MSFIsModified() {return m_Modified;};
   
@@ -78,62 +64,20 @@ public:
 	/** Remove the vme from the tree. */
   void VmeRemove(mafNode *n);
 
-	/** Get the time bounds of the tree. */
-  void TimeGetBounds(double *min, double *max);
-
-	/** Set the time for the tree. */
-  void TimeSet(double time);
-
 	/** Send the event VME_REMOVING to inform logic that the vme and its subtree are removed from the tree. */
   void NotifyRemove(mafNode *n);
 
 	/** Send the event VME_ADDED to inform logic that the vme and its subtree are added to the tree. */
   void NotifyAdd(mafNode *n);
-  
-	/** Show the dialog to confirm the save of the current tree. */
-	bool AskConfirmAndSave();
- 
-	/** Set the filename for the current tree. */
-  void SetFileName (const mafString& filename) {m_MsfFile = filename;};
- 
-	/** Get the filename of the current tree. */
-  const mafString& GetFileName () {return m_MsfFile;};
- 
-	/** Link to the main menù the file history manager. */
-  void SetFileHistoryMenu(wxMenu *menu);
- 
 	/** Return the tree's root. */
-  mafNodeRoot *GetRoot() { return m_Root;};		
-
-  /** Set the application stamp; this is the mark of the specific vertical application (is often equal to the application name). */
-  void SetApplicationStamp(const mafString& appstamp);
-  void SetApplicationStamp(const std::vector<mafString>& appstamp);
-
-	/** Set the flag for .bak file generation on saving .msf file. */
-	void MakeBakFileOnSave(bool bakfile = true) {m_MakeBakFile = bakfile;}
-
-  /** Update vme client data interface from tag. if vme == NULL, the update is propagated from root vme to all the tree. */
-  void UpdateFromTag(mafNode *n = NULL);
-
+  mafNodeRoot *GetRoot();
+   /** Set the tree's root. */
+  bool SetRoot(mafNodeRoot *root);
 protected:
-  bool m_Modified;
+  bool                m_Modified;         ///< Used to known when the tree has been modified...
+  mafAutoPointer<mafNodeRoot> m_Root;
 
-  mafNodeRoot     *m_Root;
-  wxConfigBase		*m_Config;
-  mafGUIFileHistory	 m_FileHistory;
-
-  bool      m_MakeBakFile;
-  mafString  m_MsfDir;
-  mafString  m_Wildc;
-  std::vector<mafString>  m_AppStamp;
-  mafString  m_MsfFile;
-  mafString  m_ZipFile;
-  mafString  m_MergeFile;
-
-  /*
-  bool m_Crypting;
-  mafNodeStorage    *m_storage;
-
-  */
+  /** friend test class */
+  friend class mafNodeManagerTest; // Losi 02/16/2010 for test class
 };
 #endif
