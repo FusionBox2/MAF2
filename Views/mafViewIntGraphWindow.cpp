05dcb27e1825c73fc2f793b0299a576d4e71e4d6
Fedor Moiseev <fedormsv@gmail.com>
2021-02-23 08:46:57 +0100

    WXWIN_COMPATIBILITY_2_4 0

--- Views/mafViewIntGraphWindow.cpp
+++ Views/mafViewIntGraphWindow.cpp
@@ -1123,7 +1123,7 @@ void mafViewIntGraphWindow::OnCommand(wxCommandEvent& event)
   else if(event.GetId() == ID__SAVEIMAGE)
   {
     wxString fileName = ::wxFileSelector("Select image file name", "", "image", ".bmp", "BMP files (*.bmp)|*.bmp|All files (*.*)|*.*", 
-                                 wxSAVE | wxOVERWRITE_PROMPT | wxHIDE_READONLY, this);
+                                 wxSAVE | wxOVERWRITE_PROMPT, this);
     if(fileName != "")
     {
       wxBitmap pBitMap = GetBitmap();
@@ -1133,7 +1133,7 @@ void mafViewIntGraphWindow::OnCommand(wxCommandEvent& event)
   else if(event.GetId() == ID__SAVECSV)
   {
     wxString CSVFileName = ::wxFileSelector("Select Excel CSV file name", "", "data", ".csv", "CSV files (*.csv)|*.csv|All files (*.*)|*.*", 
-                                   wxSAVE | wxOVERWRITE_PROMPT | wxHIDE_READONLY, this);
+                                   wxSAVE | wxOVERWRITE_PROMPT, this);
     if(CSVFileName != "")
     {
       if(!SaveGraphAsCSV(CSVFileName))
