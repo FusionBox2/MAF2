6144ac140b60181a9cc9ea90d1a6d0e104116bc9
Fedor Moiseev <fedormsv@gmail.com>
2021-05-03 09:50:36 +0200

    mafGui VTK6

--- Gui/mafPics.cpp
+++ Gui/mafPics.cpp
@@ -312,10 +312,8 @@ int foo = mafAddPic(vme_xpm);  //correct, but will be called before ThePicMap ha
 
       vtkImageData *vtkimg;
       vtkNEW(vtkimg);
-      vtkimg->SetNumberOfScalarComponents(NumberOfComponents);
-      vtkimg->SetScalarTypeToUnsignedChar();
       vtkimg->SetDimensions( img.GetWidth(), img.GetHeight(), 1 );
-      vtkimg->SetUpdateExtentToWholeExtent();
+      vtkimg->AllocateScalars(VTK_UNSIGNED_CHAR, NumberOfComponents);
       assert( vtkimg->GetPointData() );
       vtkimg->GetPointData()->SetScalars(buffer);
       return  vtkimg; // SHOULD BE DELETED BY THE CALLER 
