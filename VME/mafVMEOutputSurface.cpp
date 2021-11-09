1d42a3241ab90ee9ea24c59ff4e4b9bb534f7718
tjerbi <tahajerbi@yahoo.com>
2021-11-09 17:52:49 +0100

    fix crash

--- VME/mafVMEOutputSurface.cpp
+++ VME/mafVMEOutputSurface.cpp
@@ -130,7 +130,7 @@ void mafVMEOutputSurface::Update()
   if (GetSurfaceData())
   {
     //GetSurfaceData()->Modified();	//BES: 12.9.2012 - I do not see any reason for this except to make troubles during rendering since this forces rerender of everything
-    this->Update();
+    //this->Update();
     int num = GetSurfaceData()->GetNumberOfPolys();
     m_NumTriangles = mafToString(num);
   }
