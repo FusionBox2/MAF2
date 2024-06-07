2514efab7f1966db2b70341e7a64bff47db0dcfe
Fedor Moiseev <4365210+fedormsv@users.noreply.github.com>
2024-06-07 06:49:27 +0200

    warnings fix

--- Operations/medOpVolumeResample.cpp
+++ Operations/medOpVolumeResample.cpp
@@ -663,7 +663,7 @@ void medOpVolumeResample::CreateGui()
 	buildHelpGui.SetId(GET_BUILD_HELP_GUI);
 	mafEventMacro(buildHelpGui);
 
-	if (buildHelpGui.GetArg() == true)
+	if (buildHelpGui.GetArg())
 	{
 		m_Gui->Button(ID_HELP, _R("Help"), _R(""));
 	}
