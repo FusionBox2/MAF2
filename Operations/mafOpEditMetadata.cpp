16bd7557c3bc1b380812f4da0670d3871af0934e
Fedor Moiseev <4365210+fedormsv@users.noreply.github.com>
2024-06-25 22:02:03 +0200

    fix warning

--- Operations/mafOpEditMetadata.cpp
+++ Operations/mafOpEditMetadata.cpp
@@ -109,7 +109,7 @@ void mafOpEditMetadata::OpRun()
 	buildHelpGui.SetId(GET_BUILD_HELP_GUI);
 	mafEventMacro(buildHelpGui);
 
-	if (buildHelpGui.GetArg() == true)
+	if (buildHelpGui.GetArg())
 	{
 		m_Gui->Button(ID_HELP, _R("Help"), _R(""));
 	}
