701e6adbf6901bc1ae9b9c1d79a4fde0b0a515e7
Fedor Moiseev <4365210+fedormsv@users.noreply.github.com>
2024-09-16 11:59:47 +0200

    Rework MainFrame

--- Gui/mafGUIDockSettings.cpp
+++ Gui/mafGUIDockSettings.cpp
@@ -22,9 +22,9 @@
 // Failing in doing this will result in a run-time error saying:
 // "Failure#0: The value of ESP was not properly saved across a function call"
 //----------------------------------------------------------------------------
+#include <wx/aui/aui.h>
 
 #include "mafGUIDockSettings.h"
-#include "mafGUIDockManager.h"
 #include "mafGUI.h"
 #include "mafGUIDialog.h"
 
