cdfc3b970ced8b86b22d7d0d6cdd425d332b49ae
Fedor Moiseev <4365210+fedormsv@users.noreply.github.com>
2024-09-30 00:01:25 +0200

    Cleanup

--- Core/mafAttribute.h
+++ Core/mafAttribute.h
@@ -1,73 +1,44 @@
-/*=========================================================================
+#pragma once
 
- Program: MAF2
- Module: mafAttribute
- Authors: Marco Petrone
- 
- Copyright (c) B3C
- All rights reserved. See Copyright.txt or
- http://www.scsitaly.com/Copyright.htm for details.
-
- This software is distributed WITHOUT ANY WARRANTY; without even
- the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
- PURPOSE.  See the above copyright notice for more information.
-
-=========================================================================*/
-#ifndef __mafAttribute_h
-#define __mafAttribute_h
-//----------------------------------------------------------------------------
-// includes :
-//----------------------------------------------------------------------------
-#include "mafReferenceCounted.h" 
+#include "ftk/Base/Object.h"
 #include "ftk/Base/String.h"
+#include "mafReferenceCounted.h" 
 #include "mafTo.h"
 
 class mafStorageElement;
 class mafStorageElementBuilder;
 
-/** An abstract class for objects representing an attribute for mafNodes.
-  This abstract class represent the interface of an attribute for mafNodes. An attribute
-  is a bunch of data that can be attached to a node. Attributes can be specialized classes
-  providing any kind of information and functionality. The minimal required features are
-  RTTI, object factory registration and Storability. Any attribute can be serialized and 
-  then recreated from the factory and unserialized.
-  @sa mafNode mafStorable mafObject
-*/
-class MAF_EXPORT mafAttribute : public mafReferenceCounted
+class FTK_CORE_EXPORT mafAttribute : public mafReferenceCounted
 {
 public:
   mafAbstractTypeMacro(mafAttribute,mafReferenceCounted);
 
   static mafAttribute* Create(const char* AttributeType);
-  /** attributes must define a copy rule */
-  void operator=(const mafAttribute &a);
+
+  mafAttribute& operator=(const mafAttribute &a);
 
   bool operator==(const mafAttribute &a) const;
 
-  /** copy the content of the given attribute. Attributes must be type compatible */
   virtual void DeepCopy(const mafAttribute *a);
 
-  /** create a copy of this attribute */
   mafAttribute *MakeCopy();
 
-  /** return true if this attribute equals the given one */
   bool Equals(const mafAttribute *a) const;
 
-  /** 
-    Set the name of this attribute. this is typically used to identify it
-    inside the Attributes associative map stored inside a mafNode */
   void SetName(const mafString& name);
-  const mafString& GetName() const;
 
-  /** dump the object to output stream */
+	const mafString& GetName() const;
+
   void Print(std::ostream& os, const int tabs=0) const override;
 
   void Store(mafStorageElementBuilder& element) { InternalStore(element); }
-  void Restore(const mafStorageElement& element) { InternalRestore(element); }
+
+	void Restore(const mafStorageElement& element) { InternalRestore(element); }
 
 protected:
   virtual void InternalStore(mafStorageElementBuilder& parent);
-  virtual void InternalRestore(const mafStorageElement& node);
+
+	virtual void InternalRestore(const mafStorageElement& node);
 
   mafString m_Name;
 };
@@ -98,6 +69,3 @@ namespace serializer
     attr->Store(value);
   }
 }
-
-#endif
-
