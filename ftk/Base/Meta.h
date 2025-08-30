67293ab7b00d35784bcceaf639deb228f77ef356
Fedor Moiseev <4365210+fedormsv@users.noreply.github.com>
2025-08-30 12:55:47 +0200

    extend meta

--- ftk/Base/Meta.h
+++ ftk/Base/Meta.h
@@ -3,6 +3,7 @@
 #include "ftkConfigure.h"
 
 #include <type_traits>
+#include <iterator>
 
 BEGIN_FTK_NAMESPACE
 
@@ -43,6 +44,58 @@ namespace meta
 	/// `signed char` and `unsigned char` are integer types
 	template <typename T>
 	inline constexpr bool isInteger = std::is_integral_v<T> && !isCharacter<T> && !std::is_same_v<T, bool>;
+}
+namespace meta
+{
+	namespace impl {
+
+		template <typename T>
+		using KeyType = typename T::key_type;
+
+		template <typename T>
+		using MappedType = typename T::mapped_type;
+
+		/// Helps in definitions of traits for utils::meta::IsDetected
+		template <typename T, typename U>
+		using ExpectSame = std::enable_if_t<std::is_same_v<T, U>>;
+		template <typename T>
+		using IsRange =
+			ExpectSame<std::decay_t<decltype(begin(std::declval<T&>()))>, std::decay_t<decltype(end(std::declval<T&>()))>>;
+		template <typename T>
+		using SubscriptOperatorResult = decltype(std::declval<T>()[std::declval<typename T::key_type>()]);
+		template <typename T>
+		using IteratorType = std::enable_if_t<isDetected<IsRange, T>, decltype(begin(std::declval<T&>()))>;
+		template <template <typename...> typename Trait, typename... Args>
+		using DetectedType = typename impl::Detector<NotDetected, void, Trait, Args...>::type;
+		template <typename T>
+		using RangeValueType = typename std::iterator_traits<DetectedType<IteratorType, T>>::value_type;
+	}
+	template <template <typename...> typename Trait, typename... Args>
+	using DetectedType = typename impl::Detector<NotDetected, void, Trait, Args...>::type;
+
+	template <typename T>
+	using MapKeyType = DetectedType<impl::KeyType, T>;
+
+	template <typename T>
+	using MapValueType = DetectedType<impl::MappedType, T>;
+
+	template <typename T>
+	using RangeValueType = DetectedType<impl::RangeValueType, T>;
+
+
+	template <typename T>
+	inline constexpr bool kIsRange = isDetected<impl::IsRange, T>;
+	template <typename T>
+	inline constexpr bool kIsMap =
+		isDetected<impl::IsRange, T>&& isDetected<impl::KeyType, T>&& isDetected<impl::MappedType, T>;
+
+	/// Returns true if T is a map (but not a multimap!)
+	template <typename T>
+	inline constexpr bool kIsUniqueMap = kIsMap<T> && isDetected<
+		impl::SubscriptOperatorResult,
+		T>;  // no operator[] in multimaps
+
+
 }
 
 END_FTK_NAMESPACE
