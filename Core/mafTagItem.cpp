fa5cecac5fd5c488c32732d0b79ec3929d5d190d
Fedor Moiseev <4365210+fedormsv@users.noreply.github.com>
2025-10-16 23:12:54 +0200

    cleanup

--- Core/mafTagItem.cpp
+++ Core/mafTagItem.cpp
@@ -6,319 +6,317 @@
 
 namespace model::data
 {
-    TagItem::TagItem()
-    {
-        Initialize();
-    }
+	TagItem::TagItem()
+	{
+		Initialize();
+	}
 
-    TagItem::~TagItem()
-    {
-    }
+	TagItem::~TagItem() = default;
 
-    TagItem::TagItem(const mafString& name, const mafString& value, int t)
-    {
-        Initialize();
-        SetValue(value);
-        SetName(name);
-        SetType(t);
-    }
+	TagItem::TagItem(const mafString& name, const mafString& value, int t)
+	{
+		Initialize();
+		SetValue(value);
+		SetName(name);
+		SetType(t);
+	}
 
-    TagItem::TagItem(const mafString& name, const mafString* values, int numcomp, int t)
-    {
-        Initialize();
-        SetName(name);
-        SetValues(values, numcomp);
-        SetType(t);
-    }
+	TagItem::TagItem(const mafString& name, const mafString* values, int numcomp, int t)
+	{
+		Initialize();
+		SetName(name);
+		SetValues(values, numcomp);
+		SetType(t);
+	}
 
-    TagItem::TagItem(const mafString& name, const std::vector<mafString>& values, int t)
-    {
-        Initialize();
-        SetName(name);
-        SetValues(values);
-        SetType(t);
-    }
+	TagItem::TagItem(const mafString& name, const std::vector<mafString>& values, int t)
+	{
+		Initialize();
+		SetName(name);
+		SetValues(values);
+		SetType(t);
+	}
 
-    TagItem::TagItem(const mafString& name, double value)
-    {
-        Initialize();
-        SetName(name);
-        SetValue(value);
-    }
+	TagItem::TagItem(const mafString& name, double value)
+	{
+		Initialize();
+		SetName(name);
+		SetValue(value);
+	}
 
-    TagItem::TagItem(const mafString& name, const double* value, int numcomp)
-    {
-        Initialize();
-        SetName(name);
-        SetValues(value, numcomp);
+	TagItem::TagItem(const mafString& name, const double* value, int numcomp)
+	{
+		Initialize();
+		SetName(name);
+		SetValues(value, numcomp);
 
-    }
+	}
 
-    TagItem::TagItem(const mafString& name, const std::vector<double>& values)
-    {
-        Initialize();
-        SetName(name);
-        SetValues(values);
-        SetType(MAF_NUMERIC_TAG);
-    }
+	TagItem::TagItem(const mafString& name, const std::vector<double>& values)
+	{
+		Initialize();
+		SetName(name);
+		SetValues(values);
+		SetType(MAF_NUMERIC_TAG);
+	}
 
-    void TagItem::DeepCopy(const TagItem* item)
-    {
-        assert(item);
-        SetName(item->GetName());
-        SetComponents(item->GetComponents());
-        SetType(item->GetType());
-    }
+	void TagItem::DeepCopy(const TagItem* item)
+	{
+		assert(item);
+		SetName(item->GetName());
+		SetComponents(item->GetComponents());
+		SetType(item->GetType());
+	}
 
-    TagItem& TagItem::operator=(const TagItem& p)
-    {
-        DeepCopy(&p);
-        return *this;
-    }
+	TagItem& TagItem::operator=(const TagItem& p)
+	{
+		DeepCopy(&p);
+		return *this;
+	}
 
-    TagItem::TagItem(const TagItem& p)
-    {
-        Initialize();
-        *this = p;
-    }
+	TagItem::TagItem(const TagItem& p)
+	{
+		Initialize();
+		*this = p;
+	}
 
-    bool TagItem::operator==(const TagItem& p) const
-    {
-        return Equals(&p);
-    }
+	bool TagItem::operator==(const TagItem& p) const
+	{
+		return Equals(&p);
+	}
 
-    bool TagItem::operator!=(const TagItem& p) const
-    {
-        return !Equals(&p);
-    }
+	bool TagItem::operator!=(const TagItem& p) const
+	{
+		return !Equals(&p);
+	}
 
-    void TagItem::SetName(const mafString& name)
-    {
-        m_Name = name;
-    }
+	void TagItem::SetName(const mafString& name)
+	{
+		m_Name = name;
+	}
 
-    const mafString& TagItem::GetName() const
-    {
-        return m_Name;
-    }
+	const mafString& TagItem::GetName() const
+	{
+		return m_Name;
+	}
 
-    void TagItem::SetValue(double value, int component)
-    {
-        mafString tmp = mafToString(value);
-        SetValue(tmp, component);
-        SetType(MAF_NUMERIC_TAG);
-    }
+	void TagItem::SetValue(double value, int component)
+	{
+		mafString tmp = mafToString(value);
+		SetValue(tmp, component);
+		SetType(MAF_NUMERIC_TAG);
+	}
 
-    void TagItem::SetComponent(const mafString& value, int component)
-    {
-        SetValue(value, component);
-    }
+	void TagItem::SetComponent(const mafString& value, int component)
+	{
+		SetValue(value, component);
+	}
 
-    void TagItem::SetComponent(double value, int component)
-    {
-        SetValue(value, component);
-    }
+	void TagItem::SetComponent(double value, int component)
+	{
+		SetValue(value, component);
+	}
 
-    void TagItem::SetValue(const mafString& value, int component)
-    {
-        if (component >= GetNumberOfComponents())
-            SetNumberOfComponents(component + 1);
-        m_Components[component] = value;
-        SetType(MAF_STRING_TAG);
-    }
+	void TagItem::SetValue(const mafString& value, int component)
+	{
+		if (component >= GetNumberOfComponents())
+			SetNumberOfComponents(component + 1);
+		m_Components[component] = value;
+		SetType(MAF_STRING_TAG);
+	}
 
-    void TagItem::SetValues(const mafString* values, int numcomp)
-    {
-        // if the number of component differs, reallocate memory
-        if (numcomp != GetNumberOfComponents())
-        {
-            this->SetNumberOfComponents(numcomp);
-        }
+	void TagItem::SetValues(const mafString* values, int numcomp)
+	{
+		// if the number of component differs, reallocate memory
+		if (numcomp != GetNumberOfComponents())
+		{
+			this->SetNumberOfComponents(numcomp);
+		}
 
-        // copy data
-        for (int i = 0; i < numcomp; i++)
-        {
-            this->SetValue(values[i], i);
-        }
-    }
+		// copy data
+		for (int i = 0; i < numcomp; i++)
+		{
+			this->SetValue(values[i], i);
+		}
+	}
 
-    void TagItem::SetValues(const std::vector<mafString>& values)
-    {
-        m_Components = values;
-    }
+	void TagItem::SetValues(const std::vector<mafString>& values)
+	{
+		m_Components = values;
+	}
 
-    void TagItem::SetValues(const double* values, int numcomp)
-    {
-        // if the number of component differs, reallocate memory
-        if (numcomp != GetNumberOfComponents())
-        {
-            this->SetNumberOfComponents(numcomp);
-        }
+	void TagItem::SetValues(const double* values, int numcomp)
+	{
+		// if the number of component differs, reallocate memory
+		if (numcomp != GetNumberOfComponents())
+		{
+			this->SetNumberOfComponents(numcomp);
+		}
 
-        // copy data
-        for (int i = 0; i < numcomp; i++)
-        {
-            this->SetValue(values[i], i);
-        }
-    }
+		// copy data
+		for (int i = 0; i < numcomp; i++)
+		{
+			this->SetValue(values[i], i);
+		}
+	}
 
-    void TagItem::SetValues(const std::vector<double>& values)
-    {
-        // if the number of component differs, reallocate memory
-        if (values.size() != GetNumberOfComponents())
-        {
-            this->SetNumberOfComponents(values.size());
-        }
+	void TagItem::SetValues(const std::vector<double>& values)
+	{
+		// if the number of component differs, reallocate memory
+		if (values.size() != GetNumberOfComponents())
+		{
+			this->SetNumberOfComponents(values.size());
+		}
 
-        // copy data
-        for (int i = 0; i < values.size(); i++)
-        {
-            this->SetValue(values[i], i);
-        }
-    }
+		// copy data
+		for (int i = 0; i < values.size(); i++)
+		{
+			this->SetValue(values[i], i);
+		}
+	}
 
-    void TagItem::SetComponents(const mafString* values, int numcomp)
-    {
-        SetValues(values, numcomp);
-    }
+	void TagItem::SetComponents(const mafString* values, int numcomp)
+	{
+		SetValues(values, numcomp);
+	}
 
-    void TagItem::SetComponents(const std::vector<mafString>& components)
-    {
-        SetValues(components);
-    }
+	void TagItem::SetComponents(const std::vector<mafString>& components)
+	{
+		SetValues(components);
+	}
 
-    void TagItem::SetComponents(const double* components, int numcomp)
-    {
-        SetValues(components, numcomp);
-    }
+	void TagItem::SetComponents(const double* components, int numcomp)
+	{
+		SetValues(components, numcomp);
+	}
 
-    void TagItem::SetComponents(const std::vector<double>& components)
-    {
-        SetValues(components);
-    }
+	void TagItem::SetComponents(const std::vector<double>& components)
+	{
+		SetValues(components);
+	}
 
-    const mafString& TagItem::GetValue(int component) const
-    {
-        static mafString empty;
-        if (GetNumberOfComponents() > component)
-            return m_Components[component];
-        return empty;
-    }
+	const mafString& TagItem::GetValue(int component) const
+	{
+		static mafString empty;
+		if (GetNumberOfComponents() > component)
+			return m_Components[component];
+		return empty;
+	}
 
-    void TagItem::RemoveValue(int component)
-    {
-        if (GetNumberOfComponents() > component)
-            m_Components.erase(m_Components.begin() + component);
-    }
+	void TagItem::RemoveValue(int component)
+	{
+		if (GetNumberOfComponents() > component)
+			m_Components.erase(m_Components.begin() + component);
+	}
 
-    const mafString& TagItem::GetComponent(int comp) const
-    {
-        return GetValue(comp);
-    }
+	const mafString& TagItem::GetComponent(int comp) const
+	{
+		return GetValue(comp);
+	}
 
-    double TagItem::GetValueAsDouble(int component) const
-    {
-        if (GetNumberOfComponents() <= component)
-            return 0;
-        return atof(GetValue(component).GetCStr());
-    }
+	double TagItem::GetValueAsDouble(int component) const
+	{
+		if (GetNumberOfComponents() <= component)
+			return 0;
+		return atof(GetValue(component).GetCStr());
+	}
 
-    double TagItem::GetComponentAsDouble(int comp) const
-    {
-        return GetValueAsDouble(comp);
-    }
+	double TagItem::GetComponentAsDouble(int comp) const
+	{
+		return GetValueAsDouble(comp);
+	}
 
-    void TagItem::Initialize()
-    {
-        m_Name = _R("");
-        m_Type = MAF_MISSING_TAG;
-        m_Components.clear();
-    }
+	void TagItem::Initialize()
+	{
+		m_Name = _R("");
+		m_Type = MAF_MISSING_TAG;
+		m_Components.clear();
+	}
 
-    int TagItem::GetNumberOfComponents() const
-    {
-        return m_Components.size();
-    }
+	int TagItem::GetNumberOfComponents() const
+	{
+		return m_Components.size();
+	}
 
-    void TagItem::SetNumberOfComponents(int n)
-    {
-        m_Components.resize(n);
-    }
+	void TagItem::SetNumberOfComponents(int n)
+	{
+		m_Components.resize(n);
+	}
 
-    mafString TagItem::GetValueAsSingleString() const
-    {
-        if (GetNumberOfComponents() > 0)
-        {
-            mafString str = _R("(");
+	mafString TagItem::GetValueAsSingleString() const
+	{
+		if (GetNumberOfComponents() > 0)
+		{
+			mafString str = _R("(");
 
-            for (int i = 0; i < GetNumberOfComponents(); i++)
-            {
-                // colon separator
-                if (i > 0) str += _R(",");
+			for (int i = 0; i < GetNumberOfComponents(); i++)
+			{
+				// colon separator
+				if (i > 0) str += _R(",");
 
-                str += _R("\"") + m_Components[i] + _R("\"");
-            }
-            str += _R(")");
-            return str;
-        }
-        return _R("");
-    }
+				str += _R("\"") + m_Components[i] + _R("\"");
+			}
+			str += _R(")");
+			return str;
+		}
+		return _R("");
+	}
 
-    mafString TagItem::GetTypeAsString() const
-    {
-        switch (m_Type)
-        {
-        case (MAF_MISSING_TAG):
-            return _R("MIS");
-        case (MAF_NUMERIC_TAG):
-            return _R("NUM");
-        case (MAF_STRING_TAG):
-            return _R("STR");
-        }
-        return _R("UNK");
-    }
+	mafString TagItem::GetTypeAsString() const
+	{
+		switch (m_Type)
+		{
+		case (MAF_MISSING_TAG):
+			return _R("MIS");
+		case (MAF_NUMERIC_TAG):
+			return _R("NUM");
+		case (MAF_STRING_TAG):
+			return _R("STR");
+		}
+		return _R("UNK");
+	}
 
-    bool TagItem::Equals(const TagItem* item) const
-    {
-        if (!item)
-            return false;
-        return m_Name == item->m_Name && m_Type == item->m_Type && m_Components == item->m_Components;
-    }
+	bool TagItem::Equals(const TagItem* item) const
+	{
+		if (!item)
+			return false;
+		return m_Name == item->m_Name && m_Type == item->m_Type && m_Components == item->m_Components;
+	}
 
-    void TagItem::Print(std::ostream& os, int tabs) const
-    {
-        mafIndent indent(tabs);
+	void TagItem::Print(std::ostream& os, int tabs) const
+	{
+		mafIndent indent(tabs);
 
-        os << indent << "Name: \"" << (m_Name.GetCStr() ? m_Name.GetCStr() : _R("(NULL)")) << "\"";
+		os << indent << "Name: \"" << (m_Name.GetCStr() ? m_Name.GetCStr() : _R("(NULL)")) << "\"";
 
-        int t = this->GetType();
-        const char* tstr;
+		int t = this->GetType();
+		const char* tstr;
 
-        switch (t)
-        {
-        case (MAF_MISSING_TAG):
-            tstr = "MISSING";
-            break;
-        case (MAF_NUMERIC_TAG):
-            tstr = "NUMERIC";
-            break;
-        case (MAF_STRING_TAG):
-            tstr = "STRING";
-            break;
-        default:
-            tstr = "UNKNOWN";
-        }
+		switch (t)
+		{
+		case (MAF_MISSING_TAG):
+			tstr = "MISSING";
+			break;
+		case (MAF_NUMERIC_TAG):
+			tstr = "NUMERIC";
+			break;
+		case (MAF_STRING_TAG):
+			tstr = "STRING";
+			break;
+		default:
+			tstr = "UNKNOWN";
+		}
 
 
-        os << " Type: " << tstr << " (" << t << ") ";
+		os << " Type: " << tstr << " (" << t << ") ";
 
-        os << " Components: ";
+		os << " Components: ";
 
-        if (GetNumberOfComponents() > 0)
-        {
-            os << GetValueAsSingleString().GetCStr();
-            os << " NumComp: " << GetNumberOfComponents();
-        }
-        os << std::endl; // end of single line printing
-    }
+		if (GetNumberOfComponents() > 0)
+		{
+			os << GetValueAsSingleString().GetCStr();
+			os << " NumComp: " << GetNumberOfComponents();
+		}
+		os << std::endl; // end of single line printing
+	}
 }
--- Core/mafTagItem.h
+++ Core/mafTagItem.h
@@ -12,131 +12,131 @@ BEGIN_FTK_NAMESPACE
 
 namespace model::data
 {
-    class FTK_CORE_EXPORT TagItem final
-    {
-    public:
-        TagItem();
-        ~TagItem();
+	class FTK_CORE_EXPORT TagItem final
+	{
+	public:
+		TagItem();
+		~TagItem();
 
-        enum TAG_IDS { MAF_MISSING_TAG = 0, MAF_NUMERIC_TAG, MAF_STRING_TAG };
-        /** Constructors for with implicit Tag type...*/
-        TagItem(const mafString& name, const mafString& value, int t = MAF_STRING_TAG);
-        TagItem(const mafString& name, const mafString* values, int numcomp, int t = MAF_STRING_TAG);
-        TagItem(const mafString& name, const std::vector<mafString>& values, int t = MAF_STRING_TAG);
-        TagItem(const mafString& name, double value);
-        TagItem(const mafString& name, const double* value, int numcomp);
-        TagItem(const mafString& name, const std::vector<double>& values);
+		enum TAG_IDS { MAF_MISSING_TAG = 0, MAF_NUMERIC_TAG, MAF_STRING_TAG };
+		/** Constructors for with implicit Tag type...*/
+		TagItem(const mafString& name, const mafString& value, int t = MAF_STRING_TAG);
+		TagItem(const mafString& name, const mafString* values, int numcomp, int t = MAF_STRING_TAG);
+		TagItem(const mafString& name, const std::vector<mafString>& values, int t = MAF_STRING_TAG);
+		TagItem(const mafString& name, double value);
+		TagItem(const mafString& name, const double* value, int numcomp);
+		TagItem(const mafString& name, const std::vector<double>& values);
 
-        TagItem(const TagItem& p);
-        TagItem& operator=(const TagItem& p);
+		TagItem(const TagItem& p);
+		TagItem& operator=(const TagItem& p);
 
-        bool operator==(const TagItem& p) const;
-        bool operator!=(const TagItem& p) const;
+		bool operator==(const TagItem& p) const;
+		bool operator!=(const TagItem& p) const;
 
-        bool Equals(const TagItem* item) const;
+		bool Equals(const TagItem* item) const;
 
-        void DeepCopy(const TagItem* item);
+		void DeepCopy(const TagItem* item);
 
-        const mafString& GetName() const;
+		const mafString& GetName() const;
 
-        void SetName(const mafString& name);
+		void SetName(const mafString& name);
 
-        void SetValue(const mafString& value, int component = 0);
+		void SetValue(const mafString& value, int component = 0);
 
-        void SetComponent(const mafString& value, int component = 0);
+		void SetComponent(const mafString& value, int component = 0);
 
-        void SetValue(double value, int component = 0);
+		void SetValue(double value, int component = 0);
 
-    	void SetComponent(double value, int component = 0);
+		void SetComponent(double value, int component = 0);
 
-        void SetComponents(const mafString* values, int numcomp);
+		void SetComponents(const mafString* values, int numcomp);
 
-        void SetComponents(const std::vector<mafString>& components);
+		void SetComponents(const std::vector<mafString>& components);
 
-        void SetValues(const std::vector<mafString>& values);
+		void SetValues(const std::vector<mafString>& values);
 
-        void SetValues(const mafString* values, int numcomp);
+		void SetValues(const mafString* values, int numcomp);
 
-        void RemoveValue(int component);
+		void RemoveValue(int component);
 
-        void SetValues(const double* values, int numcomp);
+		void SetValues(const double* values, int numcomp);
 
-        void SetValues(const std::vector<double>& values);
+		void SetValues(const std::vector<double>& values);
 
-        void SetComponents(const double* components, int numcomp);
+		void SetComponents(const double* components, int numcomp);
 
-        void SetComponents(const std::vector<double>& components);
+		void SetComponents(const std::vector<double>& components);
 
-        const mafString& GetValue(int component = 0) const;
+		const mafString& GetValue(int component = 0) const;
 
-        const mafString& GetComponent(int comp) const;
+		const mafString& GetComponent(int comp) const;
 
-        double GetValueAsDouble(int component = 0) const;
+		double GetValueAsDouble(int component = 0) const;
 
-    	double GetComponentAsDouble(int comp) const;
+		double GetComponentAsDouble(int comp) const;
 
-        const std::vector<mafString>& GetComponents() const { return m_Components; }
+		const std::vector<mafString>& GetComponents() const { return m_Components; }
 
-        mafString GetValueAsSingleString() const;
+		mafString GetValueAsSingleString() const;
 
-        int GetType() const { return m_Type; }
+		int GetType() const { return m_Type; }
 
-        void SetType(int t) { m_Type = t; }
+		void SetType(int t) { m_Type = t; }
 
-        mafString GetTypeAsString() const;
+		mafString GetTypeAsString() const;
 
-        int GetNumberOfComponents() const;
+		int GetNumberOfComponents() const;
 
-    	void SetNumberOfComponents(int n);
+		void SetNumberOfComponents(int n);
 
-        void Print(std::ostream& os, int tabs = 0) const;
+		void Print(std::ostream& os, int tabs = 0) const;
 
-    protected:
+	protected:
 
-        void Initialize();
+		void Initialize();
 
-        mafString m_Name;
-        int m_Type;
-        std::vector<mafString> m_Components;
-    };
+		mafString m_Name;
+		int m_Type;
+		std::vector<mafString> m_Components;
+	};
 
-    template<class Value>
-    TagItem Parse(const Value& value, io::parse::To<TagItem>)
-    {
-        mafString type = value(_R("Type")).As<mafString>();
+	template<class Value>
+	TagItem Parse(const Value& value, io::parse::To<TagItem>)
+	{
+		mafString type = value(_R("Type")).As<mafString>();
 
-        int typeValue = 0;
-        if (type == _R("NUM"))
-        {
-          typeValue = TagItem::MAF_NUMERIC_TAG;
-        }
-        else if (type == _R("STR"))
-        {
-            typeValue = TagItem::MAF_STRING_TAG;
-        }
-        else if (type == _R("MIS"))
-        {
-            typeValue = TagItem::MAF_MISSING_TAG;
-        }
-        else
-        {
-        }
-        mafID num = value(_R("Mult")).As<mafID>();
-        return TagItem(value(_R("Name")).As<mafString>(), value[_R("TItem")][_R("TC")].As<std::vector<mafString>>(), typeValue);
-    }
+		int typeValue = 0;
+		if (type == _R("NUM"))
+		{
+			typeValue = TagItem::MAF_NUMERIC_TAG;
+		}
+		else if (type == _R("STR"))
+		{
+			typeValue = TagItem::MAF_STRING_TAG;
+		}
+		else if (type == _R("MIS"))
+		{
+			typeValue = TagItem::MAF_MISSING_TAG;
+		}
+		else
+		{
+		}
+		mafID num = value(_R("Mult")).As<mafID>();
+		return TagItem(value(_R("Name")).As<mafString>(), value[_R("TItem")][_R("TC")].As<std::vector<mafString>>(), typeValue);
+	}
 
-    template<class Value>
-    void Serialize(Value& value, const TagItem& item)
-    {
-        value(_R("Name")).SetValue(item.GetName());
-        value(_R("Mult")).SetValue(item.GetNumberOfComponents());
-        value(_R("Type")).SetValue(item.GetTypeAsString());
-    	auto TItem = value[_R("TItem")];
-        for (auto& comp : item.GetComponents())
-        {
-            TItem[_R("TC")][Value::npos].SetValue(comp);
-        }
-    }
+	template<class Value>
+	void Serialize(Value& value, const TagItem& item)
+	{
+		value(_R("Name")).SetValue(item.GetName());
+		value(_R("Mult")).SetValue(item.GetNumberOfComponents());
+		value(_R("Type")).SetValue(item.GetTypeAsString());
+		auto TItem = value[_R("TItem")];
+		for (auto& comp : item.GetComponents())
+		{
+			TItem[_R("TC")][Value::npos].SetValue(comp);
+		}
+	}
 
 }
 
