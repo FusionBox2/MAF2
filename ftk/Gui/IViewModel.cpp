8019d6ce30485728543d0850332e2301f9989905
Fedor Moiseev <4365210+fedormsv@users.noreply.github.com>
2026-07-03 19:02:08 +0200

    Start common Gui with IViewModel

---
+++ ftk/Gui/IViewModel.cpp
@@ -0,0 +1,10 @@
+#include "IViewModel.h"
+
+BEGIN_FTK_NAMESPACE
+
+namespace gui
+{
+	IViewModel::~IViewModel() = default;
+}
+
+END_FTK_NAMESPACE
---
+++ ftk/Gui/IViewModel.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "ftkConfigure.h"
+
+BEGIN_FTK_NAMESPACE
+
+namespace gui
+{
+	class IViewModel
+	{
+	public:
+		virtual ~IViewModel();
+	};
+}
+
+END_FTK_NAMESPACE
