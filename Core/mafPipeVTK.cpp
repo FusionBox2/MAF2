e6b9054d90844ae27383f8be49a4a69269083281
Fedor Moiseev <4365210+fedormsv@users.noreply.github.com>
2026-07-03 14:41:20 +0200

    cleanup mafPipeVTK

--- Core/mafPipeVTK.cpp
+++ Core/mafPipeVTK.cpp
@@ -1,77 +1,32 @@
-/*=========================================================================
-  Program:   Multimod Application Framework
-  Module:    $RCSfile: mafPipe.cpp,v $
-  Language:  C++
-  Date:      $Date: 2010-11-10 16:52:01 $
-  Version:   $Revision: 1.16.2.2 $
-  Authors:   Silvano Imboden
-==========================================================================
-  Copyright (c) 2002/2004
-  CINECA - Interuniversity Consortium (www.cineca.it) 
-=========================================================================*/
-
-
-#include "mafDefines.h" 
-//----------------------------------------------------------------------------
-// NOTE: Every CPP file in the MAF must include "mafDefines.h" as first.
-// This force to include Window,wxWidgets and VTK exactly in this order.
-// Failing in doing this will result in a run-time error saying:
-//----------------------------------------------------------------------------
-
+#include "mafPipeVTK.h"
 
 #include "mafSceneNode.h"
 #include "mafSceneGraph.h"
-#include "mafGUI.h"
-
-#include "mafPipeVTK.h"
 #include "mafViewVTK.h"
-#include "mafTransformBase.h"
 
-//----------------------------------------------------------------------------
-mafCxxTypeMacro(mafPipeVTK);
-//----------------------------------------------------------------------------
+mafPipeVTK::mafPipeVTK() = default;
 
-//----------------------------------------------------------------------------
-mafPipeVTK::mafPipeVTK()
-//----------------------------------------------------------------------------
+mafPipeVTK::~mafPipeVTK() = default;
+
+void mafPipeVTK::Create(mafNode* node, mafView* view)
 {
-  m_Vme           = NULL;
-	m_AssemblyFront	= NULL;
-	m_AssemblyBack	= NULL;
-	m_AlwaysVisibleAssembly = NULL;
-	m_RenFront      = NULL;
-	m_RenBack       = NULL;
-	m_AlwaysVisibleRenderer = NULL;
-}
-//----------------------------------------------------------------------------
-void mafPipeVTK::Create(mafNode *node, mafView *view)
-//----------------------------------------------------------------------------
-{
-  Superclass::Create(node, view);
-  mafViewVTK *viewVTK = mafViewVTK::SafeDownCast(m_View);
-  if(viewVTK)
-  {
-    m_RenFront      = viewVTK->GetFrontRenderer();
-    m_RenBack       = viewVTK->GetBackRenderer();
-    m_AlwaysVisibleRenderer = viewVTK->GetAlwaysVisibleRenderer();
-  }
+	Superclass::Create(node, view);
+	m_Vme = mafVME::SafeDownCast(m_Node);
 
-  m_Vme = mafVME::SafeDownCast(m_Node);
-  mafSceneGraph *sg = viewVTK->GetSceneGraph();
-  mafSceneNode  *n  = NULL;
-  if(sg)
-    n = sg->Vme2Node(m_Vme);
-  if(n)
-  {
-    m_AssemblyFront         = n->m_AssemblyFront;
-    m_AssemblyBack          = n->m_AssemblyBack;
-    m_AlwaysVisibleAssembly = n->m_AlwaysVisibleAssembly;
-  }
+	if (auto viewVTK = mafViewVTK::SafeDownCast(m_View))
+	{
+		m_RenFront = viewVTK->GetFrontRenderer();
+		m_RenBack = viewVTK->GetBackRenderer();
+		m_AlwaysVisibleRenderer = viewVTK->GetAlwaysVisibleRenderer();
 
-  m_Selected = false;
-}
-//----------------------------------------------------------------------------
-mafPipeVTK::~mafPipeVTK()
-//----------------------------------------------------------------------------
-{
+		if (auto sg = viewVTK->GetSceneGraph())
+		{
+			if (auto n = sg->Vme2Node(m_Vme))
+			{
+				m_AssemblyFront = n->m_AssemblyFront;
+				m_AssemblyBack = n->m_AssemblyBack;
+				m_AlwaysVisibleAssembly = n->m_AlwaysVisibleAssembly;
+			}
+		}
+	}
 }
--- Core/mafPipeVTK.h
+++ Core/mafPipeVTK.h
@@ -1,63 +1,33 @@
-/*=========================================================================
-  Program:   Multimod Application Framework
-  Module:    $RCSfile: mafPipe.h,v $
-  Language:  C++
-  Date:      $Date: 2010-11-10 16:52:01 $
-  Version:   $Revision: 1.18.2.3 $
-  Authors:   Silvano Imboden
-==========================================================================
-  Copyright (c) 2002/2004
-  CINECA - Interuniversity Consortium (www.cineca.it) 
-=========================================================================*/
-#ifndef __mafPipeVTK_H__
-#define __mafPipeVTK_H__
+#pragma once
 
-//----------------------------------------------------------------------------
-// Include:
-//----------------------------------------------------------------------------
 #include "mafDecl.h" // for MINID
 #include "mafPipe.h"
 #include "vtkMAFAssembly.h"
 #include "mafVME.h"
 #include "vtkRenderer.h"
 
-//----------------------------------------------------------------------------
-// forward references :
-//----------------------------------------------------------------------------
-
-//----------------------------------------------------------------------------
-// mafPipe :
-//----------------------------------------------------------------------------
-/*
-  mafPipe is the base class for all visual pipes; each pipe represents how a vme can be
-  visualized inside the view, so logically a pipe has as input a vme and in output
-  creates actors that will be rendered in a render view.
-  It can handle a GUI, which events can be catched by OnEvent.
-*/
 class MAF_EXPORT mafPipeVTK : public mafPipe
 {
 public:
-  mafTypeMacro(mafPipeVTK,mafPipe);
-  
-  mafPipeVTK();
-  ~mafPipeVTK() override;
+	mafTypeMacro(mafPipeVTK, mafPipe)
 
-  /** The real setup must be performed here - not in the ctor */
-  void Create(mafNode *node, mafView *view) override;
+	mafPipeVTK();
 
-  /** Get assembly front/back */
-  virtual vtkMAFAssembly *GetAssemblyFront(){return m_AssemblyFront;};
-  virtual vtkMAFAssembly *GetAssemblyBack(){return m_AssemblyBack;};
+	~mafPipeVTK() override;
 
-  mafVME         *m_Vme;//it is for compatibility purposes, will be removed later;
-	vtkMAFAssembly *m_AssemblyFront; ///< Assembly used to contain the actor in the front layer
-	vtkMAFAssembly *m_AssemblyBack;  ///< Assembly used to contain the actor in the background layer
-	vtkMAFAssembly *m_AlwaysVisibleAssembly;
-  bool            m_Selected; ///< Flag used to say if the rendered VME is selected.
-  vtkRenderer    *m_RenFront; ///< Renderer associated to the front layer
-  vtkRenderer    *m_RenBack;  ///< Renderer associated to the background layer
-  vtkRenderer		 *m_AlwaysVisibleRenderer; /// < Renderer used to superimpose utility stuff to main render window
+	void Create(mafNode* node, mafView* view) override;
 
 protected:
+	virtual vtkMAFAssembly* GetAssemblyFront() { return m_AssemblyFront; }
+
+	virtual vtkMAFAssembly* GetAssemblyBack() { return m_AssemblyBack; }
+
+	mafVME* m_Vme = nullptr;//it is for compatibility purposes, will be removed later;
+	bool m_Selected = false; ///< Flag used to say if the rendered VME is selected.
+	vtkMAFAssembly* m_AssemblyFront = nullptr; ///< Assembly used to contain the actor in the front layer
+	vtkMAFAssembly* m_AssemblyBack = nullptr;  ///< Assembly used to contain the actor in the background layer
+	vtkMAFAssembly* m_AlwaysVisibleAssembly = nullptr;
+	vtkRenderer* m_RenFront = nullptr; ///< Renderer associated to the front layer
+	vtkRenderer* m_RenBack = nullptr;  ///< Renderer associated to the background layer
+	vtkRenderer* m_AlwaysVisibleRenderer = nullptr; /// < Renderer used to superimpose utility stuff to main render window
 };
-#endif
